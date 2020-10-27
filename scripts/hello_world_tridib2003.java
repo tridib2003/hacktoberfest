40b3f8c9dc19053190ea0c8e122afe985cf7f882
John Doe <johndoe@example.com>
2020-10-27 22:05:10 +0530

    Added hello_world_tridib2003.java

---
+++ scripts/hello_world_tridib2003.java
@@ -0,0 +1,13 @@
+// LANGUAGE: JAVA
+// AUTHOR: Tridib Samanta
+// GITHUB: https://github.com/tridib2003
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+
+	}
+
+}
